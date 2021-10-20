67d35693672d0f3e6cebeee9d4cbdf6be3a81f28
ray.tan <ray.tan@ikala.tv>
2021-10-20 14:27:05 +0800

    check dagster version

--- dagster_slack_webhook/__init__.py
+++ dagster_slack_webhook/__init__.py
@@ -5,6 +5,6 @@ from .resources import slack_resource
 from .sensors import make_slack_on_pipeline_failure_sensor, make_slack_on_run_failure_sensor
 from .version import __version__
 
-check_dagster_package_version("dagster-slack", __version__)
+check_dagster_package_version("dagster-slack-webhook", __version__)
 
 __all__ = ["slack_resource"]
